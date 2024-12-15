42e3df549d8b0402c3835d7eb36f026751d47222
Nitin Chaurasia <gs.nitin@gmail.com>
2024-12-14 21:31:09 -0700

    Summary of changes:

--- src/components/DashboardNav.js
+++ src/components/DashboardNav.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaPlus, FaMoneyCheckAlt } from 'react-icons/fa';
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  FaPlusCircle,
+  FaRegCalendarAlt,
+  FaMoneyBillWave,
+  FaUserPlus,
+} from "react-icons/fa";
 
 const DashboardNav = () => {
-    return(
+  return (
     <nav className="dashboard-nav">
-    <Link to="/add-money" className="icon-link">
-        <FaPlus size={30} />
+      <Link to="/add-money" className="icon-link">
+        <FaPlusCircle size={30} style={{ color: "#28a745" }} />
         <span>Add Money</span>
-    </Link>
-    <Link to="/monthly-accounting" className="icon-link">
-        <FaMoneyCheckAlt size={30} />
+      </Link>
+
+      <Link to="/monthly-accounting" className="icon-link">
+        <FaRegCalendarAlt size={30} style={{ color: "#007bff" }} />
         <span>Monthly Accounting</span>
-    </Link>
-    <Link to="/withdraw" className="icon-link">
-        <FaMoneyCheckAlt size={30} />
+      </Link>
+
+      <Link to="/withdraw" className="icon-link">
+        <FaMoneyBillWave size={30} style={{ color: "#dc3545" }} />
         <span>Withdraw</span>
-    </Link>
-    <Link to="/add-account" className="icon-link">
-        <FaPlus size={30} />
+      </Link>
+
+      <Link to="/add-account" className="icon-link">
+        <FaUserPlus size={30} style={{ color: "#007bff" }} />
         <span>Add Account</span>
-    </Link>
+      </Link>
     </nav>
-    )
+  );
 };
 
 export default DashboardNav;
